cf5b7e2c535b38783259010eebdcb7d1c873d861
Gvaleriano-E <81780458+Gvaleriano-E@users.noreply.github.com>
2021-05-18 20:15:51 -0500

    finalizando dia 2

---
+++ Dia2/01-condicionales-bucle.py
@@ -0,0 +1,38 @@
+edad =input("ingresa tu edad:")
+print(type(edad))
+# para convertir datos simplemente indicamos a que tipo de datos queremos convertie,
+# y si se puede realizara exitosa,ente, sino, lanzara un KeError
+edadEntero=int(edad)
+print (type(edadEntero))
+edad = int(input("ingresa otra vex tu edad:")
+)
+
+print(float(10))
+
+# condicione
+# IF(SI) ELSE(SINO)
+restriccion_edad =18
+if edadEntero > restriccion_edad:
+    print("Eres mayor de edad, ya puedes viajar")
+   
+else:
+    print (" Eres menor de edad o puedes hacer nada")
+
+    print ("Yo soy el codigo comun y corriente")
+    
+
+# ingresar un numero y que diga si es positivo y negativo
+numero = int(input("Ingresa el numero:")
+)
+if numero < 0 :
+  print("el numero es negativo")
+ elif numero > 0:
+   print("el numero es positivo")
+
+
+
+
+
+
+
+
