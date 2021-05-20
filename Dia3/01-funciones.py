83724f61faf1a30eb9aec1b86c25f61742517177
Gvaleriano-E <81780458+Gvaleriano-E@users.noreply.github.com>
2021-05-19 19:25:49 -0500

    finalizando dia 3

---
+++ Dia3/01-funciones.py
@@ -0,0 +1,6 @@
+#Una funcion es un bloque de codigo que se puede reutilzar
+def Saludar():
+    print("Hola amigos buneas tardes")
+    """ Funcion q te  saluda cordialmente  """
+Saludar()
+print("Ya es algo tarde")
